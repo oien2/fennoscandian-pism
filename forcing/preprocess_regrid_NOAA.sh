dc6886dca011f6134579029f6e62d86efe5a151f
Rachel Oien <oien2@illinois.edu>
2015-07-24 19:36:40 -0500

    updated names so there are no longer duplicates

--- forcing/preprocess_regrid_NOAA.sh
+++ forcing/preprocess_regrid_NOAA.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#This script is calling the NOAA Seasonal European measured data
+
 
 # Adjust bounding box to accomodate 20km grids
 xmin=$((-22500 - 15000)) 
@@ -6,7 +8,7 @@ ymin=$((5547500 - 10000))
 xmax=$((2387500 + 15000))
 ymax=$((8247500 + 10000))
 
-for GRID in 20000 5000; do
+for GRID in 20000 10000 5000; do
     for var in air_temp precipitation; do
     outfile=${var}_${GRID}m.nc
     gdalwarp -overwrite -r bilinear -dstnodata 20 -s_srs EPSG:4326 -t_srs EPSG:32632 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID -of netCDF NETCDF:modernclimate.nc:${var} $outfile
